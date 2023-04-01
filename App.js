bfc2192ea69cf1e04eadcd4aae5b2f2da243f90f
codethrx <tatheermehdi2000@gmail.com>
2023-04-01 16:51:02 +0500

    App done worst layout ever developed by me XD

--- App.js
+++ App.js
@@ -1,96 +1,100 @@
-import { useState } from "react";
-import { View, Text, TextInput, Pressable, ScrollView } from "react-native";
-import tailwind from "twrnc";
-
-export default function SignupScreen() {
-  const [showPassword, setShowPassword] = useState(false);
-  const [error, setError] = useState(null);
-
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Pressable,
+  FlatList,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React from "react";
+import tw from "twrnc";
+import { StatusBar } from "expo-status-bar";
+const App = () => {
+  const [text, setText] = React.useState("");
+  const [goals, setGoals] = React.useState([]);
+  console.log(goals);
   return (
-    <ScrollView
-      contentContainerStyle={tailwind`flex-1 items-center justify-center`}
-      style={tailwind` bg-black`}
-    >
-      {error ? (
-        <View
-          style={tailwind`absolute top-8 w-full bg-red-400 mx-8 max-w-sm p-4 rounded-md`}
-        >
-          <Text style={tailwind`text-white text-medium font-bold`}>
-            Email addresses don't match
-          </Text>
-        </View>
-      ) : null}
-      <View style={tailwind`p-8 w-full max-w-sm`}>
-        <Text
-          style={tailwind`text-4xl bg-[#23d998] rounded-lg p-4 font-bold mb-6 text-white`}
-        >
-          Sign up
-        </Text>
-
-        <TextInput
-          style={tailwind`w-full bg-white rounded-md h-12 px-4 mb-4`}
-          placeholderTextColor="#000"
-          placeholder="Enter email address"
-          autoCapitalize="none"
-          keyboardType="email-address"
-        />
-
-        <TextInput
-          style={tailwind`w-full bg-white rounded-md h-12 px-4 mb-4`}
-          placeholderTextColor="#000"
-          placeholder="Confirm email address"
-          autoCapitalize="none"
-          keyboardType="email-address"
-        />
-        <TextInput
-          style={tailwind`w-full bg-white rounded-md h-12 px-4 mb-4`}
-          placeholderTextColor="#000"
-          placeholder="Confirm email address"
-          autoCapitalize="none"
-          keyboardType="email-address"
-        />
-
-        <TextInput
-          style={tailwind`w-full bg-white rounded-md h-12 px-4 mb-4`}
-          placeholderTextColor="#000"
-          placeholder="Enter password"
-          secureTextEntry={!showPassword}
-        />
-        <TextInput
-          style={tailwind`w-full bg-white rounded-md h-12 px-4 mb-4`}
-          placeholderTextColor="#000"
-          placeholder="Enter password"
-          secureTextEntry={!showPassword}
-        />
-        <TextInput
-          style={tailwind`w-full bg-white rounded-md h-12 px-4`}
-          placeholderTextColor="#000"
-          placeholder="Confirm password"
-          secureTextEntry={!showPassword}
-        />
-
-        <View style={tailwind`flex-row items-center my-8`}>
+    <View style={styles.appContainer}>
+      <View style={tw`flex-1 items-center justify-evenly`}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            value={text}
+            onChangeText={(val) => setText(val)}
+            style={tw`px-2 flex-1 border-[0.5] border-[#cccccc]`}
+            placeholder="Goal..."
+            color="#ccc"
+            cursorColor={`#ccc`}
+            placeholderTextColor="#ccc"
+          />
           <Pressable
-            style={tailwind`flex items-center justify-center bg-white h-6 w-6 rounded-sm mr-3`}
+            style={tw`flex items-center justify-center `}
+            onPress={() => {
+              setGoals([...new Set([...goals, text])]);
+              setText("");
+            }}
           >
-            <View style={tailwind`bg-green-400 h-4 w-4 rounded-sm`} />
-          </Pressable>
-          <Text style={tailwind`text-white`}>
-            I've read and agree to the terms and conditions and the privacy
-            policy
-          </Text>
-        </View>
-
-        <Pressable
-          style={tailwind`h-12 border-2 border-white rounded-md flex flex-row justify-center items-center px-6`}
-        >
-          <View style={tailwind`flex-1 flex items-center`}>
-            <Text style={tailwind`text-white text-base font-medium`}>
-              Sign up
+            <Text style={tw`bg-[#23d996] text-base h-8 text-white p-1`}>
+              Add a Goal
             </Text>
-          </View>
-        </Pressable>
+          </Pressable>
+        </View>
+        <View style={tw`h-[0.5] w-full bg-[#ccc]`} />
       </View>
-    </ScrollView>
+      <View style={tw`flex-5 bg-red-500 `}>
+        <Text style={tw`text-xl text-[#ccc]`}>Your List of Goals...</Text>
+        <ScrollView>
+          {goals.map((item) => (
+            <TouchableOpacity
+              style={tw`rounded-lg`}
+              onPress={() => setGoals(goals.filter((g) => g !== item))}
+            >
+              <Text
+                style={tw`flex text-base text-white my-4 p-4 border-[0.5] border-[#cccccc] rounded-lg`}
+              >
+                {item}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+        {/* {goals.length > 0 ? (
+          <FlatList
+            data={goals}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                style={tw`rounded-lg`}
+                onPress={() => setGoals(goals.filter((g) => g !== item))}
+              >
+                <Text
+                  style={tw` flex text-base text-white my-1 p-2 border-[0.5] border-[#cccccc] rounded-lg`}
+                >
+                  {item}
+                </Text>
+              </TouchableOpacity>
+            )}
+            keyExtractor={(item) => item}
+          />
+        ) : (
+          <Text style={tw`text-base text-white`}>No goals rn</Text>
+        )} */}
+      </View>
+      <StatusBar />
+    </View>
   );
-}
+};
+const styles = StyleSheet.create({
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 15,
+    backgroundColor: "#1b1b1b",
+    flex: 1,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+
+    // backgroundColor: "pink",
+  },
+});
+export default App;
