fc97af4c7298e13c937fcb8c96c8dbc480114232
codethrx <tatheermehdi2000@gmail.com>
2023-04-01 18:13:20 +0500

    navigation starter

--- App.js
+++ App.js
@@ -1,89 +1,13 @@
+import { View, Text } from "react-native";
 import React from "react";
-import {
-  View,
-  KeyboardAvoidingView,
-  TextInput,
-  StyleSheet,
-  Text,
-  Platform,
-  TouchableWithoutFeedback,
-  Button,
-  Keyboard,
-  ScrollView,
-} from "react-native";
-
-const KeyboardAvoidingComponent = () => {
+import { SafeAreaView } from "react-native-safe-area-context";
+import Route from "./screens/route";
+const App = () => {
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={styles.container}
-    >
-      <ScrollView>
-        <Text style={styles.header}>Header</Text>
-        <TextInput
-          placeholder="Username"
-          style={styles.textInput}
-          placeholderTextColor="#ccc"
-        />
-        <TextInput
-          placeholder="Username"
-          style={styles.textInput}
-          placeholderTextColor="#ccc"
-        />
-        <TextInput
-          placeholder="Username"
-          style={styles.textInput}
-          placeholderTextColor="#ccc"
-        />
-        <TextInput
-          placeholder="Username"
-          style={styles.textInput}
-          placeholderTextColor="#ccc"
-        />
-        <TextInput
-          placeholder="Username"
-          style={styles.textInput}
-          placeholderTextColor="#ccc"
-        />
-        <TextInput
-          placeholder="Username"
-          style={styles.textInput}
-          placeholderTextColor="#ccc"
-        />
-        <TextInput placeholder="Username" style={styles.textInput} />
-        <TextInput placeholder="Username" style={styles.textInput} />
-        <TextInput placeholder="Username" style={styles.textInput} />
-        <Button title="lolx" />
-      </ScrollView>
-    </KeyboardAvoidingView>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Route />
+    </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    paddingTop: 50,
-  },
-  inner: {
-    padding: 24,
-    flex: 1,
-    justifyContent: "space-around",
-  },
-  header: {
-    fontSize: 36,
-    marginBottom: 48,
-  },
-  textInput: {
-    height: 40,
-    borderColor: "#000000",
-    borderBottomWidth: 1,
-    marginBottom: 26,
-  },
-  btnContainer: {
-    backgroundColor: "white",
-    marginTop: 12,
-  },
-});
-
-export default KeyboardAvoidingComponent;
+export default App;
---
+++ screens/route.jsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import { View, Text, Button, ScrollView } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import tw from "twrnc";
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        onPress={() => {
+          navigation.navigate("Items", { payload: "Title payload..." });
+        }}
+        title="Push to a new screen baby"
+      />
+    </View>
+  );
+}
+function Items({ route }) {
+  return (
+    <View style={{ flex: 1 }}>
+      <ScrollView>
+        <Text style={tw`text-xl text-black`}>{route.params.payload}</Text>
+        {[
+          "a",
+          "b",
+          "c",
+          "d",
+          "e",
+          "f",
+          "g",
+          1,
+          2,
+          3,
+          5,
+          6,
+          7,
+          "az",
+          "de",
+          "ss",
+        ].map((elem) => (
+          <Text style={{ backgroundColor: "gray", padding: 20 }} key={elem}>
+            {elem}
+          </Text>
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Items" component={Items} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
